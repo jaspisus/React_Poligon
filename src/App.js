242ba08007b38edbb0b0229cb61e1779a95fca7e
jasperus <m.jackowski@outlook.com>
2021-07-30 11:43:41 +0200

    created Timer App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class ClassComponent extends React.Component {
+	state = {
+		counter: 0,
+	};
+
+	addSecond = () => {
+		this.setState({ counter: this.state.counter + 1 });
+	};
+
+	componentDidMount() {
+		setInterval(() => {
+			this.addSecond();
+		}, 1000);
+	}
+
+	render() {
+		return <h1>Class Component based counter: {this.state.counter}</h1>;
+	}
 }
 
-export default App;
+export function FunctionComponent() {
+	const [counter, updateCounter] = useState(0);
+
+	useEffect(() => {
+		setInterval(() => {
+			updateCounter(counter => counter + 1);
+		}, 1000);
+	}, []);
+
+	return <h1>Function Component based counter: {counter}</h1>;
+}
